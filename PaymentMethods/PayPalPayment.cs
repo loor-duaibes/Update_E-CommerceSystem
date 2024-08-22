57410ccb6b4583d6c96bb7dbc5ec284371fd9447
lour-duaibes <lour.duaibes7@gmail.com>
2024-08-22 12:17:09 +0300

    First commit

---
+++ PaymentMethods/PayPalPayment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.PaymentMethods
+{
+    internal class PayPalPayment : PaymentMethod
+    {
+        override
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine("Payment Process is PayPal : " + amount);
+        }
+    }
+}
